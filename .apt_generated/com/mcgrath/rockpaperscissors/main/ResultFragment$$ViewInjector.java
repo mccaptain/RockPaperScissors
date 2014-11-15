667d002c513a0a6d7335f3cdf9717b5306f0276b
Michael M <lswt65@gmail.com>
2014-11-15 15:51:34 -0700

    adding to git ignore

--- .apt_generated/com/mcgrath/rockpaperscissors/main/ResultFragment$$ViewInjector.java
+++ .apt_generated/com/mcgrath/rockpaperscissors/main/ResultFragment$$ViewInjector.java
@@ -7,32 +7,32 @@ import butterknife.ButterKnife.Finder;
 public class ResultFragment$$ViewInjector {
   public static void inject(Finder finder, final com.mcgrath.rockpaperscissors.main.ResultFragment target, Object source) {
     View view;
+    view = finder.findRequiredView(source, 2131034180, "field 'mAvatarImage'");
+    target.mAvatarImage = (android.widget.ImageView) view;
     view = finder.findRequiredView(source, 2131034185, "field 'mUserMove'");
     target.mUserMove = (android.widget.TextView) view;
+    view = finder.findRequiredView(source, 2131034191, "field 'mPlayAgainButton'");
+    target.mPlayAgainButton = (android.widget.Button) view;
+    view = finder.findRequiredView(source, 2131034188, "field 'mCpuMove'");
+    target.mCpuMove = (android.widget.TextView) view;
     view = finder.findRequiredView(source, 2131034189, "field 'mResult'");
     target.mResult = (android.widget.TextView) view;
     view = finder.findRequiredView(source, 2131034190, "field 'mRecord'");
     target.mRecord = (android.widget.TextView) view;
-    view = finder.findRequiredView(source, 2131034181, "field 'mUserName'");
-    target.mUserName = (android.widget.TextView) view;
-    view = finder.findRequiredView(source, 2131034191, "field 'mPlayAgainButton'");
-    target.mPlayAgainButton = (android.widget.Button) view;
-    view = finder.findRequiredView(source, 2131034180, "field 'mAvatarImage'");
-    target.mAvatarImage = (android.widget.ImageView) view;
-    view = finder.findRequiredView(source, 2131034188, "field 'mCpuMove'");
-    target.mCpuMove = (android.widget.TextView) view;
     view = finder.findRequiredView(source, 2131034186, "field 'mCpuAv'");
     target.mCpuAv = (android.widget.ImageView) view;
+    view = finder.findRequiredView(source, 2131034181, "field 'mUserName'");
+    target.mUserName = (android.widget.TextView) view;
   }
 
   public static void reset(com.mcgrath.rockpaperscissors.main.ResultFragment target) {
+    target.mAvatarImage = null;
     target.mUserMove = null;
+    target.mPlayAgainButton = null;
+    target.mCpuMove = null;
     target.mResult = null;
     target.mRecord = null;
-    target.mUserName = null;
-    target.mPlayAgainButton = null;
-    target.mAvatarImage = null;
-    target.mCpuMove = null;
     target.mCpuAv = null;
+    target.mUserName = null;
   }
 }
--- .apt_generated/com/mcgrath/rockpaperscissors/main/UserInputFragment$$ViewInjector.java
+++ .apt_generated/com/mcgrath/rockpaperscissors/main/UserInputFragment$$ViewInjector.java
@@ -7,26 +7,26 @@ import butterknife.ButterKnife.Finder;
 public class UserInputFragment$$ViewInjector {
   public static void inject(Finder finder, final com.mcgrath.rockpaperscissors.main.UserInputFragment target, Object source) {
     View view;
+    view = finder.findRequiredView(source, 2131034192, "field 'mUserNameEditbox'");
+    target.mUserNameEditbox = (android.widget.EditText) view;
     view = finder.findRequiredView(source, 2131034195, "field 'mInputButton'");
     target.mInputButton = (android.widget.Button) view;
-    view = finder.findRequiredView(source, 2131034193, "field 'mUserAgePicker'");
-    target.mUserAgePicker = (android.widget.NumberPicker) view;
     view = finder.findRequiredView(source, 2131034196, "field 'mUserSpinner'");
     target.mUserSpinner = (android.widget.Spinner) view;
     view = finder.findRequiredView(source, 2131034197, "field 'mLoadButton'");
     target.mLoadButton = (android.widget.Button) view;
-    view = finder.findRequiredView(source, 2131034192, "field 'mUserNameEditbox'");
-    target.mUserNameEditbox = (android.widget.EditText) view;
+    view = finder.findRequiredView(source, 2131034193, "field 'mUserAgePicker'");
+    target.mUserAgePicker = (android.widget.NumberPicker) view;
     view = finder.findRequiredView(source, 2131034194, "field 'mFemaleSexCheckBox'");
     target.mFemaleSexCheckBox = (android.widget.CheckBox) view;
   }
 
   public static void reset(com.mcgrath.rockpaperscissors.main.UserInputFragment target) {
+    target.mUserNameEditbox = null;
     target.mInputButton = null;
-    target.mUserAgePicker = null;
     target.mUserSpinner = null;
     target.mLoadButton = null;
-    target.mUserNameEditbox = null;
+    target.mUserAgePicker = null;
     target.mFemaleSexCheckBox = null;
   }
 }
